414e7ce404acf781b6e653df7b67588a24a38be4
Tiger Abrodi <tigerabrodi@gmail.com>
2025-04-13 11:12:04 +0200

    fix how we export as name spaces

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
-// Core
-export * from './core/option'
+// Namespaced exports to avoid conflicts
+import * as Either from './core/either'
+import * as List from './core/list'
+import * as Option from './core/option'
+import * as Tuple from './core/tuple'
+import * as Compose from './utils/compose'
+import * as Curry from './utils/curry'
+import * as Pipe from './utils/pipe'
+
+export { Compose, Curry, Either, List, Option, Pipe, Tuple }
